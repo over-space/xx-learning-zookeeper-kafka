cdc82cd124909dfccc07a226f28820891d3e14ee
lifang <lifang@worktrans.cn>
2021-08-27 13:55:25 +0800

    kafka提交

--- src/test/java/com/learning/kafka/test/KafkaConsumerTest.java
+++ src/test/java/com/learning/kafka/test/KafkaConsumerTest.java
@@ -1,19 +1,27 @@
 package com.learning.kafka.test;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
 public class KafkaConsumerTest {
+
+    private static final Logger logger = LogManager.getLogger(KafkaConsumerTest.class);
 
     @Test
     public void testKafkaConsumer(){
@@ -21,9 +29,34 @@ public class KafkaConsumerTest {
         configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.181.211:9092,192.168.181.212:9092,192.168.181.213:9092");
         configs.put(ConsumerConfig.GROUP_ID_CONFIG, "msn-group-1");
 
+        // 是否自动提交offset.
+        configs.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+
+        // 偏移量无效之后，采取的策略
+        // latest 最新的消息 （丢失消息风险）
+        // earliest 上一次有效的offset （重复消费风险）
+        configs.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+
         KafkaConsumer consumer = new KafkaConsumer(configs, new StringDeserializer(), new StringDeserializer());
 
-        consumer.subscribe(Arrays.asList("msn-topic-1", "msn-topic-2"));
+        // consumer.seek(new TopicPartition("msn-topic-2", 0), 4255);
+
+        consumer.subscribe(Arrays.asList("msn-topic-1", "msn-topic-2"), new ConsumerRebalanceListener() {
+
+            @Override
+            public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+                // 再均衡开始之前和消费者停止消费消息之后被调用
+                logger.info("消费者停止消费消息，即将触发再均衡....{}", partitions);
+            }
+
+            @Override
+            public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+                // 再均衡之后和消费者开始消费之前被调用
+                logger.info("已重新分配分区，消费者即将开始消费....{}", partitions);
+            }
+        });
+
+        logger.info("------------------------------------------------------------------------------------------------");
 
         while (true){
 
@@ -33,8 +66,18 @@ public class KafkaConsumerTest {
                 Iterator iterator = records.records(topic).iterator();
                 while (iterator.hasNext()) {
                     ConsumerRecord record = (ConsumerRecord)iterator.next();
-                    System.out.printf("topic = %s, partition = %d, offset = %d, key = %s, value = %s\n", topic, record.partition(), record.offset(), record.key(), record.value());
+                    logger.info("topic = {}, partition = {}, offset = {}, key = {}, value = {}", topic, record.partition(), record.offset(), record.key(), record.value());
                 }
+            }
+
+            try {
+                // 同步提交偏移量
+                // consumer.commitSync();
+
+                // 异步提交偏移量
+                consumer.commitAsync();
+            }catch (Exception e){
+                logger.error(e.getMessage(), e);
             }
         }
     }
--- src/test/java/com/learning/kafka/test/KafkaProducerTest.java
+++ src/test/java/com/learning/kafka/test/KafkaProducerTest.java
@@ -4,6 +4,8 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -14,12 +16,13 @@ import java.util.concurrent.TimeUnit;
 
 public class KafkaProducerTest {
 
+    private static final Logger logger = LogManager.getLogger(KafkaProducerTest.class);
 
     @Test
     public void testKafkaProducer() throws InterruptedException {
         Map<String, Object> configs = new HashMap<>();
         configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.181.211:9092,192.168.181.212:9092,192.168.181.213:9092");
-        configs.put(ProducerConfig.ACKS_CONFIG, "0");
+        configs.put(ProducerConfig.ACKS_CONFIG, "all");
 
         KafkaProducer producer = new KafkaProducer(configs, new StringSerializer(), new StringSerializer());
 
@@ -46,7 +49,60 @@ public class KafkaProducerTest {
                 if (i % 2 != 0) {
                     producer.send(new ProducerRecord("msn-topic-2", "key-" + i, "hello kafka-" + i));
                     try {
+                        TimeUnit.MILLISECONDS.sleep(400);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                i++;
+            }
+        });
+
+        while(true){
+            TimeUnit.MILLISECONDS.sleep(600);
+        }
+    }
+
+
+    @Test
+    public void testKafkaProducerCallback() throws InterruptedException {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.181.211:9092,192.168.181.212:9092,192.168.181.213:9092");
+        configs.put(ProducerConfig.ACKS_CONFIG, "1");
+
+        KafkaProducer producer = new KafkaProducer(configs, new StringSerializer(), new StringSerializer());
+
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+
+        logger.info("-------------------------------------------------------------------------------------------------");
+
+        executorService.execute(() -> {
+            int i = 0;
+            while (true) {
+                if(i % 2 == 0) {
+                    producer.send(new ProducerRecord("msn-topic-1", "key-" + i, "hello kafka-" + i), (metadata, exception) -> {
+                        logger.info("topic : {}, partition : {}, offset : {}", "msn-topic-1", metadata.partition(), metadata.offset());
+                    });
+                    try {
                         TimeUnit.MILLISECONDS.sleep(300);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                i++;
+            }
+        });
+
+        executorService.execute(() -> {
+            int i = 0;
+            while (true) {
+                if (i % 2 != 0) {
+                    producer.send(new ProducerRecord("msn-topic-2", "key-" + i, "hello kafka-" + i), (metadata, exception) -> {
+                        logger.info("topic : {}, partition : {}, offset : {}", "msn-topic-2", metadata.partition(), metadata.offset());
+                    });
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(350);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
